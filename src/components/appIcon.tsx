ba47d43e2a2d8c61db25d8435a508797d0b7b0db
Lee Price <leejprice@hotmail.co.uk>
2025-01-30 10:47:14 +0000

    icon update

--- src/components/appIcon.tsx
+++ src/components/appIcon.tsx
@@ -1,11 +1,18 @@
+import { motion } from "framer-motion";
 import { IconSwitchHorizontal } from "@tabler/icons-react";
 
 const SwitchIcon = () => (
-  <IconSwitchHorizontal
-    className="w-12 h-12 text-yellow-400 drop-shadow-[0_0_10px_rgba(255,215,0,0.5)] 
-               hover:text-yellow-300 transition-all 
-               duration-200 ease-in-out hover:scale-110"
-  />
+  <motion.div
+    initial={{ y: -5, opacity: 0 }}
+    animate={{ y: [0, -3, 3, -1.5, 1.5, 0], opacity: 1 }}
+    transition={{ duration: 1.2, ease: "easeInOut" }}
+  >
+    <IconSwitchHorizontal
+      className="w-10 h-10 text-yellow-400 drop-shadow-[0_0_10px_rgba(255,215,0,0.5)] 
+                 hover:text-yellow-300 transition-all 
+                 duration-200 ease-in-out hover:scale-110"
+    />
+  </motion.div>
 );
 
 export default SwitchIcon;
