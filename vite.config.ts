d1cab48ed00f3ed9bc9952d500cf2377214acad6
Lee Price <leejprice@hotmail.co.uk>
2025-02-07 13:16:07 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3003,
-    host: true, // Add this to expose to all network interfaces
+    host: true,
   },
-  root: '.',
   build: {
     outDir: 'dist'
   }
